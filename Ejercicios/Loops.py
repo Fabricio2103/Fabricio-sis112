8dd1937a886ec380ce6c3abde502dc47f4d7df4f
Fabricio2103 <fabricioesp4@gmail.com>
2024-08-21 19:09:23 -0400

    v

---
+++ Ejercicios/Loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+    j = 0
+    while j<n:
+        j=j+1
+        k=i**2
+        print(k)
+        i=i+1
---
+++ Ejercicios/Print function.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    i=1
+    j=0
+    while j<n:
+        print(i, end='')
+        i=i+1
+        j=j+1
